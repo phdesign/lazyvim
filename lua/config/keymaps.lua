402962c43ddfc0e276e3f82ead9930fe40a80dd2
Paul Heasley <paul@eql.com>
2023-09-04 19:58:35 +1000

    Adding custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "v",
+  "<leader>t",
+  ":s/\\%V\\<\\(\\w\\)\\(\\w*\\)\\>/\\u\\1\\L\\2/ge<bar>noh<cr>",
+  { desc = "Title Case Selection" }
+)
+
+vim.keymap.set("v", "gj", "<esc>:'<,'>!jq --sort-keys -c '.'<cr>", { desc = "Sort JSON" })
+
+vim.keymap.set(
+  "n",
+  "<leader>uu",
+  '"=system(\'python3 -c "import uuid; print(uuid.uuid4(), end=\\"\\");"\')<CR>p',
+  { desc = "Insert new GUID" }
+)
