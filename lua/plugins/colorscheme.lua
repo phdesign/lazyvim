7026c26177c460fed148fa6af102802feaf9202a
Paul Heasley <paul.heasley@getequiem.com>
2025-05-09 12:27:37 +1000

    Switch to GitHub theme dark dimmed

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
+  { "projekt0n/github-nvim-theme" },
   {
-    "chriskempson/tomorrow-theme",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/vim")
-      vim.cmd([[colorscheme Tomorrow-Night-Eighties]])
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_dimmed",
+    },
   },
 }
